d2bee84fcc271ee4d43052594c0065c3be0d77cf
Rley <ffast2000@gmail.com>
2024-05-08 11:17:24 -0400

    testwebhook4

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,47 +1,66 @@
-import stripe from 'stripe'
-import { NextResponse } from 'next/server'
-import { createOrder, userTicketAssign } from '@/lib/actions/orderAction'
-import { prisma } from '@/db/prisma'
+import stripe, { Stripe } from "stripe";
+import { NextResponse } from "next/server";
+import { createOrder, userTicketAssign } from "@/lib/actions/orderAction";
+import { prisma } from "@/db/prisma";
 
 export async function POST(request: Request) {
+  const body = await request.json();
 
-  const body = await request.text()
+  console.log(body);
 
-  console.log(body)
+  switch (body.type) {
+    case "checkout.session.completed": {
+      const { id, amount_total, metadata } = body.data.object;
 
-  const sig = request.headers.get('stripe-signature') as string
-  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!
+      console.log(id, amount_total);
 
-  let event
+      const order = {
+        stripeId: id,
+        eventId: metadata?.eventId || "",
+        buyerId: metadata?.buyerId || "",
+        totalAmount: amount_total ? (amount_total / 100).toString() : "0",
+        createdAt: new Date(),
+      };
 
-  try {
-    event = stripe.webhooks.constructEvent(body, sig, endpointSecret)
-  } catch (err) {
-    console.log(err)
-    return NextResponse.json({ message: 'Webhook error', error: err })
-  }
-
-  // Get the ID and type
-  const eventType = event.type
-
-  // CREATE
-  if (eventType === 'checkout.session.completed') {
-    const { id, amount_total, metadata } = event.data.object
-
-    console.log(id, amount_total)
-
-    const order = {
-      stripeId: id,
-      eventId: metadata?.eventId || '',
-      buyerId: metadata?.buyerId || '',
-      totalAmount: amount_total ? (amount_total / 100).toString() : '0',
-      createdAt: new Date(),
+      const newOrder = await createOrder(order);
+      await userTicketAssign(order);
+      return NextResponse.json({ message: "OK", order: newOrder });
     }
-
-    const newOrder = await createOrder(order)
-    await userTicketAssign(order)
-    return NextResponse.json({ message: 'OK', order: newOrder })
   }
 
-  return new Response('', { status: 200 })
+  // const sig = request.headers.get('stripe-signature') as string
+  // const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!
+
+  // let event
+
+  // try {
+  //   event = stripe.webhooks.constructEvent(body, sig, endpointSecret)
+  // } catch (err) {
+  //   console.log(err)
+  //   return NextResponse.json({ message: 'Webhook error', error: err })
+  // }
+
+  // // Get the ID and type
+  // const eventType = event.type
+
+  // // CREATE
+  // if (eventType === 'checkout.session.completed') {
+  //   const { id, amount_total, metadata } = event.data.object
+
+  //   console.log(id, amount_total)
+
+  //   const order = {
+  //     stripeId: id,
+  //     eventId: metadata?.eventId || '',
+  //     buyerId: metadata?.buyerId || '',
+  //     totalAmount: amount_total ? (amount_total / 100).toString() : '0',
+  //     createdAt: new Date(),
+  //   }
+
+  //   const newOrder = await createOrder(order)
+  //   await userTicketAssign(order)
+  //   return NextResponse.json({ message: 'OK', order: newOrder })
+  // }
+
+  // return new Response('', { status: 200 })
 }
