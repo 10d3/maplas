6e558661c0578acfdc8cee85bc675ffda06be61c
Rley <ffast2000@gmail.com>
2024-05-15 12:59:13 -0400

    dashboard cust...

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,11 +1,17 @@
 import { auth } from '@/auth/auth'
 import AreaChartComponent from '@/components/shared/areaChart';
 import AreaChart from '@/components/shared/areaChart';
+import EventCard from '@/components/shared/eventCard';
 import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
+import { prisma } from '@/db/prisma';
 import React from 'react'
 
 export default async function DashBoard() {
+  const session = await auth();
+  const user = session?.user
+
+  const events = await prisma.event.findMany({where:{createdById : user?.id}})
   const data = [
     { date: "2023-04-01", cashIn: 1926, cashOut: 1450 },
     { date: "2023-04-02", cashIn: 2994, cashOut: 2235 },
@@ -20,10 +26,8 @@ export default async function DashBoard() {
     { date: "2023-04-11", cashIn: 17000, cashOut: 11712 },
     { date: "2023-04-12", cashIn: 18000, cashOut: 12811 },
   ];
-  const session = await auth();
-  const user = session?.user
   return (
-    <div className='w-[100%] md:w-auto flex items-center flex-col justify-center gap-4'>
+    <div className='w-[100%] md:w-auto h-auto flex items-center flex-col justify-center gap-4 mb-6'>
       <section className='flex md:flex-row flex-col justify-between w-full md:w-[80%] gap-4'>
         <div className='flex justify-center items-center'>
           <h1>Welcome back {user?.name} </h1>
@@ -45,18 +49,18 @@ export default async function DashBoard() {
         ))} */}
       </section>
       <section className='flex flex-col md:flex-row w-full justify-between items-center md:w-[80%] gap-4'>
-        <Card className='w-full md:w-1/2'>
-          <CardContent>
+        <Card className='w-full md:w-1/2 h-[27rem] p-0 m-0'>
             <p className="p-4 font-semibold">Overview</p>
+          <CardContent className='w-full m-0 p-0 h-full'>
             <AreaChartComponent data={data} />
           </CardContent>
         </Card>
         <Card className='w-full md:w-1/2 h-[27rem]'>
           <CardContent className="flex flex-col justify-between gap-2">
             <p className="p-4 font-semibold">Recent Events list</p>
-            {/* {events.map(event => (
-              <EventCard event={event} />
-            ))} */}
+            {events.map(event => (
+              <EventCard key={event.id} event={event} />
+            ))}
           </CardContent>
         </Card>
       </section>
--- src/components/shared/areaChart.tsx
+++ src/components/shared/areaChart.tsx
@@ -1,20 +1,63 @@
 'use client'
 import React from 'react'
-import { Area, AreaChart, ResponsiveContainer, XAxis } from 'recharts'
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 
 export default function AreaChartComponent({ data }: any) {
-    console.log("je monte")
     return (
-        <ResponsiveContainer width='100%' height='100%'>
+        <ResponsiveContainer width='100%' height='80%'>
             <AreaChart data={data}>
-                <Area dataKey='cashIn' type='monotone' stroke='#06b6d4' />
-                <XAxis dataKey='date' stroke='#334155' />
+                <CartesianGrid strokeDasharray='2 2' stroke='#1e293b' vertical={false} />
+                <Area dataKey='cashIn' type='monotone' fill={`url(#cyan-gradient)`} stroke='#06b6d4' />
+                <XAxis dataKey='date'
+                    fontSize={10} stroke='#334155'
+                    tickLine={false} axisLine={false} interval={3}
+                    tickFormatter={(value: string) => {
+                        return value.split('-').reverse().slice(0, 2).join('/')
+                    }}
+                />
+                <YAxis
+                    dataKey='cashIn'
+                    fontSize={10} stroke='#334155'
+                    tickLine={false} axisLine={false} interval={1}
+                    tickFormatter={(value) => {
+                        return `$${Intl.NumberFormat('en-us').format(value)}`
+                    }} />
                 <defs>
-                    <linearGradient id='cyan-gradient' x1='0' y1='0' x2='' y2='1'>
-                        <stop offset='0%' stopColor='#06b6d4' stopOpacity={0.4}/>
-                        <stop offset='75%' stopColor='##d946ef' stopOpacity={0.05}/>
+                    <linearGradient id='cyan-gradient' x1='0' y1='0' x2='0' y2='1'>
+                        <stop offset='0%' stopColor='#06b6d4' stopOpacity={0.4} />
+                        <stop offset='75%' stopColor='##d946ef' stopOpacity={0.05} />
                     </linearGradient>
                 </defs>
+                <Tooltip
+                    cursor={
+                        {
+                            fill: '#06b6d4',
+                            radius: 4,
+                            stroke: '#334155'
+                        }
+                    }
+                    content={({ active, payload }) => {
+                        if (!active || !payload || payload.length === 0) {
+                            return null
+                        }
+                        return (
+                            <div className=' rounded-lg border bg-slate-800 border-slate-700 p-2 shadow-sm'>
+                                <div className='grid grid-cols-2 gap-2'>
+                                    <div className='flex flex-col'>
+                                        <span className='text-xm font-semibold uppercase text-slate-500'>Date</span>
+                                        <span className='text-sm font-bold text-slate-300'>{payload[0].payload.date}</span>
+                                    </div>
+                                    <div className='flex flex-col'>
+                                        <span className='text-xm font-semibold uppercase text-slate-500'>Cash In</span>
+                                        <span className='text-sm font-bold text-slate-300'>
+                                            ${Intl.NumberFormat('en-us').format(payload[0].payload.cashIn)}
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }}
+                />
             </AreaChart>
         </ResponsiveContainer>
     )
