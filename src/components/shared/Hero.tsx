8a8525481d733adb12bd0a98a4ecf7006dd9249b
Rley <ffast2000@gmail.com>
2024-06-04 12:55:45 -0400

    stylizing footer

--- src/components/shared/Hero.tsx
+++ src/components/shared/Hero.tsx
@@ -4,9 +4,9 @@ import EventFilterSidebar from "./EventFilterSidebar"
 
 export default function Hero() {
 
-  const slides: string[] = ["/assets/image-slider1.jpg", "/assets/image-slider2.jpg"]
+  const slides: string[] = ["/assets/image-slider1.jpg", "/assets/image-slider2.jpg",'/assets/image-slider3.jpg']
   return (
-    <section className=" h-[90vh] flex flex-col justify-around items-center ">
+    <section className=" h-auto my-10 flex flex-col justify-around items-center ">
       <div className=" w-full flex flex-col md:flex-row items-center gap-6">
         <div className="flex w-full md:w-2/4 items-center justify-center"><EventFilterSidebar /></div>
         <Carousel autoSlide={true}>
--- src/features/layout/Footer.tsx
+++ src/features/layout/Footer.tsx
@@ -1,7 +1,9 @@
 import TestSend from "@/components/test/testSend";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { SiteConfig } from "@/lib/site-config";
+import { Facebook, Instagram, Twitter } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -14,40 +16,57 @@ export default function Footer() {
         { name: "FAQ", href: "#" },
     ];
 
+    const icons = [
+        { name: <Facebook size={26} />, href: 'https://www.facebook.com/' },
+        { name: <Instagram size={26} />, href: 'https://www.instagram.com/' },
+        { name: <Twitter size={26} />, href: 'https://twitter.com/' },
+    ]
+
     return (
         <>
             <Separator className=" bg-slate-300" />
-            <footer className="w-full flex items-center justify-center flex-col md:flex-row pb-8 gap-4">
-                <div className="w-full md:w-1/3 items-center flex flex-col md:flex-row gap-4">
-                    <div><Image className="w-[7rem] h-auto" src='/logo/logo-p.png' height={1000} width={1000} alt={`${SiteConfig.title} logo`} /></div>
-                    <div className="w-full flex flex-col items-center justify-center">
-                        <h1 className="text-xl font-medium">Subcribe to our newsletter</h1>
-                        <Button className=" bg-custom-button-primary">Subcribe</Button>
-                    </div>
-                </div>
-                <div className="w-full md:w-2/3 items-center flex flex-col md:flex-row gap-4">
-                    <div className="w-full md:w-1/4 items-center flex flex-col">
-                        <h1 className="text-xl font-medium">Quick link</h1>
-                        <div className="flex flex-col items-center justify-center gap-2">
-                            {links.map((link, i) => {
-                                return (
-                                    <Link className='' key={i} href={link.href}>{link.name}</Link>
-                                );
-                            })}
+            <footer className="w-full flex items-center justify-center flex-col py-6 gap-4">
+                <div className="w-full flex items-center justify-center flex-col md:flex-row gap-4">
+                    <div className="w-full md:w-1/3 items-center flex flex-col md:flex-row gap-4">
+                        <div><Image className="w-[4rem] h-auto" src='/logo/logo-p.png' height={1000} width={1000} alt={`${SiteConfig.title} logo`} /></div>
+                        <div className="w-full flex flex-col items-center justify-center gap-4">
+                            <h1 className="text-xl font-medium">Subcribe to our newsletter</h1>
+                            <div className=" w-3/4 flex flex-col gap-2 items-center">
+                                <Input className="w-full" type="email" placeholder="email" />
+                                <Button className=" w-2/4 bg-custom-button-primary">Subcribe</Button>
+                            </div>
                         </div>
                     </div>
-                    <div className="w-full md:w-1/4 items-center flex flex-col gap-4">
-                        <h1>Quick link</h1>
-                        <div>link</div>
-                        <TestSend />
+                    <div className="w-full md:w-2/3 items-center flex flex-col md:flex-row gap-4">
+                        <div className="w-full md:w-1/3 items-center flex flex-col">
+                            <h1 className="text-xl font-medium">Quick link</h1>
+                            <div className="flex flex-col items-center justify-center gap-2">
+                                {links.map((link, i) => {
+                                    return (
+                                        <Link className='' key={i} href={link.href}>{link.name}</Link>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        <div className="w-full md:w-1/3 items-center flex flex-col gap-4">
+                            <h1 className="text-xl font-medium">Quick link</h1>
+                            <div>link</div>
+                            <TestSend />
+                        </div>
+                        <div className="w-full md:w-1/3 items-center flex flex-col gap-4">
+                            <h1 className="text-xl font-medium">Quick link</h1>
+                            <div>link</div>
+                        </div>
                     </div>
-                    <div className="w-full md:w-1/4 items-center flex flex-col gap-4">
-                        <h1>Quick link</h1>
-                        <div>link</div>
-                    </div>
-                    <div className="w-full md:w-1/4 items-center flex flex-col gap-4">
-                        <h1>Quick link</h1>
-                        <div>link</div>
+                </div>
+                <div className="w-full items-center flex flex-col gap-4">
+                    <h1 className="text-xl font-medium">Follow Us</h1>
+                    <div className="flex flex-row gap-4">
+                        {
+                            icons.map((icon, i) => {
+                                return (<Link key={i} href={icon.href}>{icon.name}</Link>)
+                            })
+                        }
                     </div>
                 </div>
             </footer>
